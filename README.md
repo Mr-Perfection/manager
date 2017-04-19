700c1b58354b1e2ee7295ecf67850eb792f5fcdd
Sungsoo lee <sungsoolee0127@gmail.com>
2017-04-19 12:55:12 -0700

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # manager
 React Native Application
+
+### Setup
+```
+$ npm install --save react-redux redux
+$ npm install --save firebase
+```
+basic setup
+```js
+import {
+  AppRegistry,
+} from 'react-native';
+import App from './src/App';
+
+AppRegistry.registerComponent('manager', () => App);
+```
---
+++ manager/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyAcaIcM8EzByLZah3_LlIvOKFAzc0b7QxE',
+      authDomain: 'manager-88f95.firebaseapp.com',
+      databaseURL: 'https://manager-88f95.firebaseio.com',
+      projectId: 'manager-88f95',
+      storageBucket: 'manager-88f95.appspot.com',
+      messagingSenderId: '959701600616'
+    };
+    firebase.initializeApp(config);
+  }
+  
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>
+            Hello!
+          </Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
